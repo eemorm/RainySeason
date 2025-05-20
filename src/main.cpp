4d1f47fe9436592ef7078d32871c3fa70e674ff2
gub_ <eemorm@gmail.com>
2025-05-20 15:55:48 -0500

    basic window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
+//Custom Includes
 #include "plant.hpp"
+
+// SFML
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+// Standard Output
 #include <iostream>
+#include <optional>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({SCREEN_WIDTH, SCREEN_HEIGHT}), "Rainy Season", sf::Style::Close);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }      
+        }
+        window.clear(sf::Color(127, 127, 127));
+        window.display();
+    }
     return 0;
 }
-
-// basic program for structure testing
