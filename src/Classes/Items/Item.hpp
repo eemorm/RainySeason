3d227c630dd25f8384f256148dd287a8db42f65a
eddyhw <eddywen11@gmail.com>
2025-05-23 19:07:58 +0000

    attempt at fixing item class

--- src/Classes/Items/Item.hpp
+++ src/Classes/Items/Item.hpp
@@ -21,16 +21,10 @@ class Item //defining the class Item
         std::string rarity; //might need to be changed if we do stars or smth
         bool isStackable; //whether the item is stackable
         int maxStackable; //if it is stackable, what the max stacking is
-
-        if (!isStackable) //if it isn't stackable, then you can only stack up to 1
-        {
-            maxStackable = 1;
-        }
-
         bool isSellable; //whether the item can be sold
-        if (isSellable) {int sellPrice;} //price if it can be sold
+        int sellPrice; //price if it can be sold
         bool isTradable; //whether it can be traded
-        if (isTradable) {std:string tradeReward} //what you get in return for trading
+        std::string tradeReward; //what you get in return for trading
     //****we might not end up implementing isSellable or isTradable!****
     public:
         Item() {} //constructer?
@@ -53,19 +47,10 @@ class Item //defining the class Item
         void setMaxStackable(int ms) {maxStackable = ms;}
         bool getIsSellable() {return isSellable;}
         void setIsSellable(bool isll) {isSellable = isll;}
-
-        if (isSellable) //only do setters and getters for the sell price if it can be sold
-        {
-            int getSellPrice() {return sellPrice;}
-            void setSellPrice(int sp) {sellPrice = sp;} 
-        }
-
+        int getSellPrice() {return sellPrice;}
+        void setSellPrice(int sp) {sellPrice = sp;} 
         bool getIsTradable() {return isTradable;}
         void setIsTradable(bool it) {isTradable = it;}
-        
-        if (isTradable) //only do trading mechanics if it can be traded
-        {
-            std::string getTradeReward() {return tradeReward;}
-            void setTradeReward(std::string tr) {tradeReward = tr;}
-        }
+        std::string getTradeReward() {return tradeReward;}
+        void setTradeReward(std::string tr) {tradeReward = tr;}
 }
