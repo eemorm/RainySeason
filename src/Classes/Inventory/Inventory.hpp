78bb279c92fc63148f7c584d7827600e7615ddfd
eddyhw <eddywen11@gmail.com>
2025-05-26 19:22:05 +0000

    added inventory class

---
+++ src/Classes/Inventory/Inventory.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+// SFML
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+// Standard Output
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Inventory //declaring inventory class
+{
+    private:
+        std::vector<Item> items; //declaring list of items of type Item class
+        int capacity; //max amount of items that can be held
+    public:
+        Inventory() {} //constructor
+        ~Inventory() {} //deconstructor
+
+        //setters and getters
+        std::vector getItems() {return items;} //returns list of items
+        void setItems(std::vector i) {items = i;}
+        //following functions are for dealing with items in the inventory
+        void insertItem(const Item& newItem, int index) //adds newItem to the list at index 
+        {
+            items.insert(items.begin() + index, newItem);
+        }
+
+        void addItem(const Item& newItem) //adds newItem to the end of the list
+        {
+            items.push_back(newItem);
+        }
+
+        void removeItem(int index) //removes item at certain index from the list
+        {
+            items.erase(items.begin() + index);
+        }
+
+        int getCapacity() {return capacity;} //returns capacity of the inventory
+        void setCapacity(int c) {capacity = c;} //sets capacity to c
+}; 
