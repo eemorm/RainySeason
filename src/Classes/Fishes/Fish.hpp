fa81946d249de0e1a4bb6543a725c493d153f6da
eddyhw <eddywen11@gmail.com>
2025-05-28 19:08:01 +0000

    added salmon subclass and changed fish class to have std::vectors

--- src/Classes/Fishes/Fish.hpp
+++ src/Classes/Fishes/Fish.hpp
@@ -10,17 +10,18 @@
 // Standard Output
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Fish // declaring a Fish class
 {
     private:
         sf::Sprite sprite; // sprite of the fish
         std::string name; // name/type of the fish
-        std::string location; // where it can be caught
+        std::vector<std::string> location; // where it can be caught
         std::string difficulty; // difficulty to catch
-        std::string season; // season(s) where it can be caught
-        std::string time; // the time that it can be caught (i.e., Morning)
-        std::string weather; // the weahter where it can be caught
+        std::vector<std::string> season; // season(s) where it can be caught
+        std::vector<std::string> timeOfDay; // Morning, Noon (maybe), Afternoon, Night 
+        std::vector<std::string> weather; // the weahter where it can be caught
         int weight; // weight of the fish
     public:
         Fish() {} // constructor
@@ -31,16 +32,16 @@ class Fish // declaring a Fish class
         void setSprite(sf::Sprite s) {sprite = s;}
         std::string getName() {return name;}
         void setName(std::string n) {name = n;}
-        std::string getLocation() {return location;}
-        void setLocation(std::string l) {location = l;}
+        std::vector<std::string> getLocation() {return location;}
+        void setLocation(std::vector<std::string> l) {location = l;}
         std::string getDifficulty() {return difficulty;}
         void setDifficulty(std::string d) {difficulty = d;}
-        std::string getSeason() {return season;}
-        void setSeason(std::string seas) {season = seas;}
-        std::string getTime() {return time;}
-        void setTime(std::string t) {time = t;}
-        std::string getWeather() {return weather;}
-        void setWeather(std::string w) {weather = w;}
+        std::vector<std::string> getSeason() {return season;}
+        void setSeason(std::vector<std::string> seas) {season = seas;}
+        std::vector<std::string> getTimeOfDay() {return timeOfDay;}
+        void setTimeOfDay(std::vector<std::string> t) {timeOfDay = t;}
+        std::vector<std::string> getWeather() {return weather;}
+        void setWeather(std::vector<std::string> w) {weather = w;}
         int getWeight() {return weight;}
         void setWeight(int w) {weight = w;}
 };
